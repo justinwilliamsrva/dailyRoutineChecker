5113458254ab29e544f54194a03daec03ccfea61
justinwilliamsrva <justin.williams2@me.com>
2021-01-05 20:58:37 -0500

    create map function for night

--- script.js
+++ script.js
@@ -20,13 +20,29 @@ let day = [
     "emails",
     "mail goals",
 ]
-
+let evening = [
+    "exercise",
+    "milk",
+    "litter",
+    "laundry",
+    "cat food and water",
+    "clean dining room",
+    "computer room",
+    "bathroom",
+    "tea",
+    "litter",
+    "kitchen",
+    "dishwasher",
+]
 // Adding to ul
 let morningList = document.getElementById("morning-list");
 let dayList = document.getElementById("day-list");
+let eveningList = document.getElementById("evening-list");
 
 morning.map(addList);
-day.map(addListDay)
+day.map(addListDay);
+evening.map(addListNight);
+
 
 function addList(item) {
     let li = document.createElement("li");
@@ -39,4 +55,10 @@ function addListDay(item) {
     li.innerHTML = item;
 
     dayList.append(li);
+}
+function addListNight(item) {
+    let li = document.createElement("li");
+    li.innerHTML = item;
+
+    eveningList.append(li);
 }
