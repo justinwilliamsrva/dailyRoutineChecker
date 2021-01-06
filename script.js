2dcd9f7ae30ce41f9b6218fde6171ca195857fc6
justinwilliamsrva <justin.williams2@me.com>
2021-01-05 20:54:05 -0500

    Added map method for DAy tasks

--- script.js
+++ script.js
@@ -15,14 +15,28 @@ let morning = [
     "Dishwasher",
     "Lunch",
 ];
+let day = [
+    "lesson plans",
+    "emails",
+    "mail goals",
+]
 
 // Adding to ul
 let morningList = document.getElementById("morning-list");
+let dayList = document.getElementById("day-list");
 
 morning.map(addList);
+day.map(addListDay)
 
 function addList(item) {
     let li = document.createElement("li");
     li.innerHTML = item;
+
     morningList.append(li);
+}
+function addListDay(item) {
+    let li = document.createElement("li");
+    li.innerHTML = item;
+
+    dayList.append(li);
 }
